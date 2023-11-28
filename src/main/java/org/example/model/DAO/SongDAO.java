5f383e0c1a4435d34d10a4e1b17d6b0fe3492455
rafa30052003 <rafalucenavalle9@gmail.com>
2023-11-28 13:22:55 +0100

    primeros cambios de SongDAO

--- src/main/java/org/example/model/DAO/SongDAO.java
+++ src/main/java/org/example/model/DAO/SongDAO.java
@@ -1,11 +1,13 @@
 package org.example.model.DAO;
 
+import org.example.conexion.Connection;
 import org.example.interfaceDAO.iDAO;
 import org.example.model.domain.Album;
 
 import org.example.model.domain.Song;
 
-import java.sql.Connection;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,41 +15,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SongDAO extends Song implements iDAO<Song, Integer> {
-    /*
-     * QUERY
-     */
-    private final static String FINDALL ="SELECT id, name_song, gender, N_reproduction, duration, name_disk, archive_song, name_disk FROM song";
-
-    private final static String FINBYID ="SELECT name_song, gender, N_reproduction, duration, name_disk,archive_song  from song WHERE id  =?";
-    private final static String INSERT ="INSERT INTO song (id, name_song, gender,  N_reproduction, duration, name_disk,archive_song) VALUES (NULL,?,?,?,?,?,?)";
-    private final static String UPDATE ="UPDATE song SET name_song = ?, gender = ?, N_reproduction = ?, duration = ?, name_disk = ? WHERE id=?";
-    private final static String DELETE ="DELETE FROM song WHERE id=?";
-
 
 
     /**
      * Conexion
      */
-    private Connection conn;
-
-
-    public SongDAO(int id, String name_song, String gender, int nrepro, String duration, Album album, String archive_song, Connection conn) {
-        super(id, name_song, gender, nrepro, duration, album, archive_song);
-        this.conn = conn;
-    }
-
-    public SongDAO(String name_song, String gender, int nrepro, String duration, Album album, String archive_song, Connection conn) {
-        super(name_song, gender, nrepro, duration, album, archive_song);
-        this.conn = conn;
-    }
-
-    public SongDAO(Connection conn) {
-        this.conn = conn;
-    }
-
-    public SongDAO() {
-        this.conn= Connect.getConnect();
-    }
+    private EntityManager manager;
 
     @Override
     public List<Song> findAll() throws SQLException {
@@ -80,76 +53,102 @@ public class SongDAO extends Song implements iDAO<Song, Integer> {
         return result;
     }
 
-
+    /**
+     * Metodo que usca una cancion por su clave
+     * @param id clave de la cancion
+     * @return la cancion a buscar
+     */
     @Override
-    public Song findById(Integer id) throws SQLException {
+    public Song findById(Integer id) {
         Song result = null;
-        try(PreparedStatement pst=this.conn.prepareStatement(FINBYID)){
-            pst.setInt(1, id);
-            try(ResultSet res = pst.executeQuery()){
-                if(res.next()) {
-                    result = new Song();
-                    result.setName_song(res.getString("name_song"));
-                    result.setGender(res.getString("gender"));
-                    result.setNrepro(res.getInt("N_reproduction"));
-                    result.setDuration(res.getString("duration"));
-                    AlbumDAO adao = new AlbumDAO(this.conn);
-                    Album a = adao.findById(res.getString("name_disk"));
-                    result.setAlbum(a);
-                    result.setArchive_song(res.getString("archive_song"));
 
+        try {
+            manager = Connection.getConnect().createEntityManager();
 
+            TypedQuery<Song> query = manager.createQuery(
+                    "SELECT s FROM Song s WHERE s.id = :id",
+                    Song.class
+            );
 
-                }
-            }
+            query.setParameter("id", id);
+
+            result = query.getSingleResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            manager.close();
         }
+
         return result;
-
     }
-
+    /**
+     * Metodo que inserta una cancion , else (la cancion si esta creada lo modifica)
+     * @param entity: La cancion a crear o modificar
+     * @return la cancion
+     */
     @Override
-    public Song save(Song entity) throws SQLException {
+    public Song save(Song entity) {
         Song result = new Song();
+
         if (entity != null) {
-            Song s = findById(entity.getId());
+            try {
+                manager = Connection.getConnect().createEntityManager();
+                manager.getTransaction().begin();
 
-            if (s == null) {
-                // INSERT
-                try (PreparedStatement pst = this.conn.prepareStatement(INSERT)) {
-                    pst.setString(1, entity.getName_song());
-                    pst.setString(2, entity.getGender());
-                    pst.setInt(3, entity.getNrepro());
-                    pst.setString(4, entity.getDuration());
-                    pst.setString(5, entity.getAlbum().getName());
-                    pst.setString(6, entity.getArchive_song());
-
-                    pst.executeUpdate();
+                if (entity.getId() == -1) {
+                    // INSERT
+                    manager.persist(entity);
+                } else {
+                    // UPDATE
+                    entity = manager.merge(entity);
                 }
 
-            } else {
-                // UPDATE
-                try (PreparedStatement pst = this.conn.prepareStatement(UPDATE)) {
-                    pst.setString(1,entity.getName_song());
-                    pst.setString(2, entity.getGender());
-                    pst.setInt(3, entity.getNrepro());
-                    pst.setString(4, entity.getDuration());
-                    pst.setString(5, entity.getAlbum().getName());
-                    pst.setInt(6,entity.getId());
+                manager.getTransaction().commit();
+                result = entity;
 
-                    pst.executeUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                if (manager.getTransaction().isActive()) {
+                    manager.getTransaction().rollback();
                 }
+            } finally {
+                manager.close();
             }
-            result = entity;
         }
+
         return result;
     }
 
-
+    /**
+     * Metodo que elimina la cancion
+     * @param entity la cancion a eliminar
+     */
     @Override
-    public void delete(Song entity) throws SQLException {
-        try(PreparedStatement pst=this.conn.prepareStatement(DELETE)){
-            pst.setInt(1, entity.getId());
-            pst.executeUpdate();
+    public void delete(Song entity) {
+        if (entity != null) {
+            try {
+                manager = Connection.getConnect().createEntityManager();
+                manager.getTransaction().begin();
+
+                // Si la entidad no está en el contexto de persistencia, adjuntarla antes de eliminar
+                if (!manager.contains(entity)) {
+                    entity = manager.merge(entity);
+                }
+
+                manager.remove(entity);
+                manager.getTransaction().commit();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                if (manager.getTransaction().isActive()) {
+                    manager.getTransaction().rollback();
+                }
+            } finally {
+                manager.close();
+            }
         }
     }
 
